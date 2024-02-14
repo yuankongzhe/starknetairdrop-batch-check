833d3af06d4d7ca82b56815d1e3c412517128195
yuankongzhe <yuankongzhe@gmail.com>
2024-02-14 17:22:36 +0800

    修复重要bug：大小写应该不区分

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,8 @@
 export function categorizeAddresses(addressList) {
     return addressList.reduce((acc, address) => {
+      if (address.startsWith('0x')) {
+        address = address.toLowerCase();
+      }
       // 取地址的前两个字符，检查是否为0x开头
       const prefix = address.startsWith('0x') ? address.slice(2, 3) : address.slice(0, 1);
       if (!acc[prefix]) {
